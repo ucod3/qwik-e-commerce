f850bfbb9ab876fedbc870a46e47c3e1889eed07
Usman Butt <usman.butt@outlook.com>
2024-08-19 14:57:27 -0400

    add supabase seed

---
+++ supabase/seed.sql
@@ -0,0 +1,9 @@
+insert into public.products
+  (name, description, price, image, slug)
+values
+  ('Spiky Cactus', 'A spiky yet elegant house cactus - perfect for the home or office. Origin and habitat: Probably native only to the Andes of Peru', 20.95,  'spiky_cactus.webp', 'spiky-cactus'),
+  ('Tulip Pot', 'Bright crimson red species tulip with black centers, the poppy-like flowers will open up in full sun. Ideal for rock gardens, pots and border edging.', 10.95, 'tulip_pot.webp', 'tulip-pot'),
+  ('Aloe Vera', 'Decorative Aloe vera makes a lovely house plant. A really trendy plant, Aloe vera is just so easy to care for.', 10.45, 'aloe_vera.webp', 'aloe-vera'),
+  ('Fern Blechnum Gibbum', 'Create a tropical feel in your home with this lush green tree fern, it has decorative leaves and will develop a short slender trunk in time.', 12.95, 'fern_blechnum_gibbum.webp', 'fern-blechnum-gibbum'),
+  ('Assorted Indoor Succulents', 'These assorted succulents come in a variety of different shapes and colours - each with their own unique personality.', 42.35, 'assorted_indoor_succulents.webp', 'assorted-indoor-succulents'),
+  ('Orchid', 'Gloriously elegant. It can go along with any interior as it is a neutral color and the most popular Phalaenopsis overall.', 30.75, 'orchid.webp', 'orchid');

a70ac0f85645087dc37d99298fd043ae49a8318a
Usman Butt <usman.butt@outlook.com>
2024-08-19 16:24:58 -0400

    update ui

--- src/routes/sign-in/index.tsx
+++ src/routes/sign-in/index.tsx
@@ -1,5 +1,5 @@
 import { component$ } from "@builder.io/qwik";
-import { Form, routeAction$, z, zod$ } from "@builder.io/qwik-city";
+import { Form, Link, routeAction$, z, zod$ } from "@builder.io/qwik-city";
 import { supabaseClient } from "~/utils/supabase";
 
 export const useSignInAction = routeAction$(
@@ -26,15 +26,53 @@ export default component$(() => {
   const signInAction = useSignInAction();
   return (
     <Form action={signInAction}>
-      <div>
-        <label for="email">Your email</label>
-        <input type="email" name="email" id="email" />
-      </div>
-      <div>
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" />
-      </div>
-      <button type="submit">Sign in</button>
+      <section class="bg-gray-50">
+        <div class="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
+          <div class="w-full rounded-lg bg-white shadow">
+            <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
+              <h1 class="text-xl font-bold leading-tight tracking-tight md:text-2xl">
+                Sign in
+              </h1>
+              <div>
+                <label for="email" class="mb-2 block text-sm font-medium">
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 focus:border-blue-600 focus:ring-blue-600 sm:text-sm"
+                />
+              </div>
+              <div>
+                <label for="password" class="mb-2 block text-sm font-medium">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 focus:border-blue-600 focus:ring-blue-600 sm:text-sm"
+                />
+              </div>
+              <button
+                type="submit"
+                class="w-full rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white outline-none ring-4 ring-blue-500 hover:bg-blue-700"
+              >
+                Sign in
+              </button>
+              <div class="w-full text-center">
+                <Link
+                  href="/sign-up"
+                  class="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                >
+                  Sign up
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </Form>
   );
 });
--- src/routes/sign-up/index.tsx
+++ src/routes/sign-up/index.tsx
@@ -1,40 +1,70 @@
-import { component$, useSignal } from '@builder.io/qwik';
-import { supabaseClient } from '../../utils/supabase';
+import { component$, useSignal } from "@builder.io/qwik";
+import { Link } from "@builder.io/qwik-city";
+import { supabaseClient } from "~/utils/supabase";
 
 export default component$(() => {
-  const emailSig = useSignal('');
-  const passwordSig = useSignal('');
-  const messagesSig = useSignal('');
-
+  const emailSig = useSignal("");
+  const passwordSig = useSignal("");
+  const messageSig = useSignal("");
   return (
-    <div class='container'>
-      <label for='email'>Email</label>
-      <input
-        type='email'
-        id='email'
-        value={emailSig.value}
-        onInput$={(_, el) => {
-          emailSig.value = el.value;
-        }}
-      />
-      <label for='password'>Password</label>
-      <input type='password' id='password' bind:value={passwordSig} />
-
-      {!!messagesSig.value && <p>{messagesSig.value}</p>}
-
-      <button
-        onClick$={async () => {
-          const { error } = await supabaseClient.auth.signUp({
-            email: emailSig.value,
-            password: passwordSig.value,
-          });
-          messagesSig.value = error
-            ? 'Error'
-            : 'Success. Please check your email/spam folder for a confirmation email.';
-        }}
-      >
-        Sign Up
-      </button>
-    </div>
+    <section class="bg-gray-50">
+      <div class="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
+        <div class="w-full rounded-lg bg-white shadow">
+          <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
+            <h1 class="text-xl font-bold leading-tight tracking-tight md:text-2xl">
+              Sign up to your account
+            </h1>
+            <div>
+              <label for="email" class="mb-2 block text-sm font-medium">
+                Your email
+              </label>
+              <input
+                type="email"
+                id="email"
+                bind:value={emailSig}
+                class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 focus:border-blue-600 focus:ring-blue-600 sm:text-sm"
+              />
+            </div>
+            <div>
+              <label for="password" class="mb-2 block text-sm font-medium">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                bind:value={passwordSig}
+                class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 focus:border-blue-600 focus:ring-blue-600 sm:text-sm"
+              />
+            </div>
+            {!!messageSig.value && (
+              <div class="w-full py-4 text-center">{messageSig.value}</div>
+            )}
+            <button
+              type="submit"
+              class="w-full rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white outline-none ring-4 ring-blue-500 hover:bg-blue-700"
+              onClick$={async () => {
+                const { error } = await supabaseClient.auth.signUp({
+                  email: emailSig.value,
+                  password: passwordSig.value,
+                });
+                messageSig.value = error
+                  ? "Error"
+                  : "Success. Please check your email/spam folder";
+              }}
+            >
+              Sign up
+            </button>
+            <div class="w-full text-center">
+              <Link
+                href="/sign-in"
+                class="font-medium text-blue-600 hover:underline dark:text-blue-500"
+              >
+                Sign in
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 });

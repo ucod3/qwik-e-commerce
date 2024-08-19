5fda8636b2fcdc4a33f43f2108f8203d88ec261e
Usman Butt <usman.butt@outlook.com>
2024-08-19 15:06:13 -0400

    add RequestHandler

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,4 +1,14 @@
-import { component$, Slot } from "@builder.io/qwik";
+import {
+  component$,
+  createContextId,
+  Slot,
+  useContextProvider,
+  useStore,
+} from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
+import { NavbarTop } from "~/components/NavbarTop";
+import type { Store } from "~/utils/store";
+import { supabaseClient } from "~/utils/supabase";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
@@ -12,6 +22,35 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
   });
 };
 
+
+
+export const useUser = routeLoader$(async (requestEv) => {
+  const supabaseAccessToken = requestEv.cookie.get("supabase_access_token");
+  if (!supabaseAccessToken) {
+    return null;
+  }
+  const { data, error } = await supabaseClient.auth.getUser(
+    supabaseAccessToken.value,
+  );
+  return error ? null : data.user;
+});
+
+export const STORE_CONTEXT = createContextId<Store>("STORE_CONTEXT");
+
+const initialData: Store = {
+  cart: {
+    products: [],
+  },
+};
+
 export default component$(() => {
-  return <Slot />;
+  const store = useStore<Store>(initialData, { deep: true });
+  useContextProvider(STORE_CONTEXT, store);
+
+  return (
+    <div>
+      <NavbarTop />
+      <Slot />
+    </div>
+  );
 });
